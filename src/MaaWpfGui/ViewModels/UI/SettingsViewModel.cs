57aeab9fa48b7611793b28d43cd500dea079ede0
status102 <102887808+status102@users.noreply.github.com>
2025-04-10 09:29:32 +0800

    chore: 先扔着吧
    perf: 默认值处理
    
    fix: rebase
    
    chore: 基建设施
    
    fix: key重复
    
    rft: 序列化调整
    
    refactor: 肉鸽任务追加
    
    fix: Guide
    
    fix: InfrastMode
    
    fix: typo
    
    rft: 生息任务补上商店购物
    
    rft: 长草任务队列
    
    rft: 基建除设施列表迁移
    
    rft: 剩余理智迁移
    
    chore: 刷理智任务添加
    
    fix: rebase
    
    rft: 公招任务
    
    fix: rebase
    
    chore: 长草任务UI基础
    
    rft: 肉鸽任务 刷开局奖励
    
    rft: 肉鸽任务 除烧水奖励
    
    rft: 生息演算任务
    
    rft: 信用任务
    
    chore: 测试代码
    
    rft: UI绑定更新
    
    perf: 简化参数
    
    fix: namespace
    
    rft: 长草任务UI base
    
    rft: 基础性修改
    
    rft: 领取奖励任务界面绑定
    
    rft: 免费单抽字段统一
    
    perf: 移除旧config中的非全局更新信息
    
    rft: 开始唤醒任务
    
    chore: 任务储存
    
    rft: 部分配置迁移
    
    半成品 #10474
    
    feat: UI刷新接口
    
    chore: 基础性调整
    
    fix: 切换配置补全
    
    rft: 整理一下目录
    
    rft: rename

--- src/MaaWpfGui/ViewModels/UI/SettingsViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/SettingsViewModel.cs
@@ -26,6 +26,7 @@ using System.Windows;
 using HandyControl.Controls;
 using HandyControl.Data;
 using JetBrains.Annotations;
+using MaaWpfGui.Configuration.Factory;
 using MaaWpfGui.Constants;
 using MaaWpfGui.Extensions;
 using MaaWpfGui.Helper;
@@ -510,6 +511,7 @@ namespace MaaWpfGui.ViewModels.UI
             {
                 SetAndNotify(ref _currentConfiguration, value);
                 ConfigurationHelper.SwitchConfiguration(value);
+                ConfigFactory.SwitchConfig(value);
 
                 Bootstrapper.ShutdownAndRestartWithoutArgs();
             }
@@ -532,7 +534,7 @@ namespace MaaWpfGui.ViewModels.UI
                 NewConfigurationName = DateTime.Now.ToString("yy/MM/dd HH:mm:ss");
             }
 
-            if (ConfigurationHelper.AddConfiguration(NewConfigurationName, CurrentConfiguration))
+            if (ConfigurationHelper.AddConfiguration(NewConfigurationName, CurrentConfiguration) && ConfigFactory.AddConfiguration(NewConfigurationName, CurrentConfiguration))
             {
                 ConfigurationList.Add(new CombinedData { Display = NewConfigurationName, Value = NewConfigurationName });
 
