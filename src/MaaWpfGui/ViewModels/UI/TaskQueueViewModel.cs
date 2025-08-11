8a2f05173dce14cecc9212104610a0c795ec833b
status102 <102887808+status102@users.noreply.github.com>
2025-08-11 23:58:13 +0800

    rft: 基建房屋状态

--- src/MaaWpfGui/ViewModels/UI/TaskQueueViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/TaskQueueViewModel.cs
@@ -1152,8 +1152,7 @@ namespace MaaWpfGui.ViewModels.UI
 
             // 虽然更改时已经保存过了，不过保险起见在点击开始之后再次保存任务和基建列表
             // TaskItemSelectionChanged();
-            InfrastTask.InfrastOrderSelectionChanged();
-
+            // InfrastTask.InfrastOrderSelectionChanged();
             await Task.Run(() => SettingsViewModel.GameSettings.RunScript("StartsWithScript"));
 
             AddLog(LocalizationHelper.GetString("ConnectingToEmulator"));
@@ -1352,8 +1351,7 @@ namespace MaaWpfGui.ViewModels.UI
 
             // 虽然更改时已经保存过了，不过保险起见在点击开始之后再次保存任务和基建列表
             // TaskItemSelectionChanged();
-            InfrastTask.InfrastOrderSelectionChanged();
-
+            // InfrastTask.InfrastOrderSelectionChanged();
             ClearLog();
 
             await Task.Run(() => SettingsViewModel.GameSettings.RunScript("StartsWithScript"));
